83e051b89614a79e4e52dc703dcd35d5f1a716e9
VijayPatil86 <vijipatil090386@gmail.com>
2025-05-19 20:23:48 +0530

    initial commit

---
+++ src/main/java/com/user_service/config/DataInitializer.java
@@ -0,0 +1,43 @@
+package com.user_service.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.user_service.entity.User;
+import com.user_service.repository.UserRepository;
+
+@Component
+public class DataInitializer implements CommandLineRunner {
+	@Autowired
+	private UserRepository userRepository;
+
+	@Value("${admin.name}")
+	private String adminName;
+
+	@Value("${admin.role}")
+	private String adminRole;
+
+	@Value("${admin.email}")
+	private String adminEmail;
+
+	@Value("${admin.password}")
+	private String adminPassword;
+
+	@Override
+	public void run(String... args) throws Exception {
+		User adminUser = userRepository.findByUserEmail(adminEmail);
+		if(adminUser == null) {
+			adminUser = User.builder()
+					.name(adminName)
+					.userEmail(adminEmail)
+					.role(adminRole)
+					.password(adminPassword)
+					.build();
+			userRepository.save(adminUser);
+		}
+
+	}
+
+}
